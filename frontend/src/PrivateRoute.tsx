4e8720a965f9daa576f122d7b6a3ce791e90fe9c
Filipe Volz <filipevolz00@gmail.com>
2025-04-24 23:52:13 -0300

    remove loading privateRoute

--- frontend/src/PrivateRoute.tsx
+++ frontend/src/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
 export function PrivateRoute() {
-  const { data, isLoading } = useQuery({
+  const { data } = useQuery({
     queryKey: ['auth-check'],
     queryFn: async () => {
       const response = await axios.get('https://projeto-faculdade-nwhu.onrender.com/auth/validate', {
@@ -13,11 +13,6 @@ export function PrivateRoute() {
     },
     retry: false,
   })
-
-  if (isLoading) {
-    return <p>Carregando...</p> // pode trocar por skeleton ou spinner
-  }
-
   if (!data?.authenticated) {
     return <Navigate to="/account/login" replace />
   }
